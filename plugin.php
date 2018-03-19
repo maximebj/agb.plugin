9171da9ff48234a347cf514d5bbd23c114bc9379
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2018-03-19 16:41:13 +0100

    updated version number

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Description: Awesome customizable blocks for the new WordPress Editor
  * Author: maximebj
  * Author URI: https://dysign.fr
- * Version: 1.0.0
+ * Version: 1.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
