ce926fad9bf2f5145b741c5f3746d55f8228111d
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2018-09-24 14:59:39 +0200

    Updated gulpfile config

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,9 +1,16 @@
 const gulp = require("gulp")
 const stylus = require("gulp-stylus")
-const autoprefixer = require("gulp-autoprefixer")
 const plumber = require("gulp-plumber")
 
-const bs = require("browser-sync").create()
+const browserSync = require("browser-sync").create()
+
+gulp.task( "browser-sync", function() {
+  browserSync.init({
+    proxy: "gutenblocks.local"
+  })
+
+  gulp.watch("./stylus/**/*.styl", ["stylus-admin-css"])
+})
 
 gulp.task("stylus-admin-css", function() {
   return gulp
@@ -15,33 +22,7 @@ gulp.task("stylus-admin-css", function() {
       })
     )
     .pipe(gulp.dest("../admin/css"))
-    .pipe(bs.reload({ stream: true }))
+    .pipe(browserSync.stream())
 })
 
-gulp.task("watch", function() {
-  gulp.watch("./stylus/**/*.styl", ["stylus-admin-css"])
-})
-
-// gulp.task("stylus-public-css", function() {
-//   return gulp
-//     .src("./stylus/gutenberg-blocks-public.styl")
-//     .pipe(plumber())
-//     .pipe(
-//       stylus({
-//         compress: true
-//       })
-//     )
-//     .pipe(autoprefixer({ browsers: ["last 2 versions"] }))
-//     .pipe(gulp.dest("../public/css"))
-//     .pipe(bs.reload({ stream: true }))
-// })
-
-
-// gulp.task("browser-sync", function() {
-//   bs.init({
-//     proxy: "http://gutenblock.local",
-//     ghostMode: false,
-//     open: false,
-//     notify: false
-//   })
-// })
+gulp.task("watch", ["browser-sync"])
