e11766e782899223dc1204f345ab1c78496d112c
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2019-01-23 13:54:18 +0100

    Fixed clicktotweet render default attributes

--- classes/Blocks/ClickToTweet.php
+++ classes/Blocks/ClickToTweet.php
@@ -44,15 +44,19 @@ class ClickToTweet {
 
 	public function render_block( $attributes ) {
 
+		// Default values
+		$content = array_key_exists( 'content', $attributes ) ? $attributes['content'] : '';
+		$hashtags =  array_key_exists( 'hashtags', $attributes ) ? $attributes['hashtags'] : '';
+
     $url  = ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http" ) . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $url = urlencode( $url );
 
     $user = $this->get_agb_or_yoast_twitter_user();
 
-    $content = str_replace( '<br/>', ' ', $attributes['content'] );
+    $content = str_replace( '<br/>', ' ', $content );
     $content = urlencode( strip_tags( $content ) );
 
-    $hashtags = urlencode( $attributes['hashtags'] );
+    $hashtags = urlencode( $hashtags );
 
     $intent_URL = "https://twitter.com/intent/tweet?url=$url&via=$user&text=$content&hashtags=$hashtags";
 
