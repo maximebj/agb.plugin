0d1332fee8d5245e45f6b0f25857179037eb1920
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2019-03-21 20:53:30 +0100

    Added babel plugin makepot and pot-to-php to handle JS string i18n

---
+++ languages/agb.php
@@ -0,0 +1,479 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: src/blocks/ad/index.js:10
+	// Reference: src/blocks/adtext/index.js:41
+	__( 'Advertisement', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/ad/index.js:11
+	// Reference: src/blocks/notice/index.js:44
+	__( 'Put forward a tips or a warning', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/ad/index.js:15
+	// Reference: src/blocks/adtext/index.js:18
+	__( 'ad', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/ad/index.js:16
+	// Reference: src/blocks/adtext/index.js:19
+	__( 'advertising', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/ad/index.js:17
+	__( 'banner', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/ad/index.js:22
+	__( 'An ad will be displayed here in front', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/index.js:15
+	__( 'Add to cart button', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/index.js:16
+	__( 'This button allow a customer to quickly add a product to cart', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/index.js:20
+	__( 'purchase', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/index.js:38
+	// Reference: src/blocks/product/preview.js:94
+	__( 'Add to cart', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/inspect.js:28
+	__( 'Add', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/inspect.js:28
+	__( 'to cart', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/inspect.js:34
+	__( 'Search Product', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/inspect.js:42
+	// Reference: src/blocks/product/inspect.js:31
+	__( 'Colors', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/inspect.js:47
+	__( 'Background Color', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/inspect.js:53
+	__( 'Icon', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/addtocart/inspect.js:64
+	__( 'Display Icon?', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/adtext/index.js:13
+	__( 'Text + Ad', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/adtext/index.js:14
+	__( 'Display a text on the left and a rectangle ad on the right', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/adtext/index.js:20
+	__( 'publicity', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/adtext/index.js:46
+	__( 'Write text here', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/index.js:13
+	// Reference: src/blocks/card/urlfetcher.js:52
+	__( 'Website card preview', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/index.js:14
+	__( 'Turn a simple url in a pretty card preview', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/index.js:18
+	__( 'url', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/index.js:19
+	// Reference: src/blocks/post/index.js:19
+	__( 'link', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/index.js:63
+	// Reference: src/blocks/giphy/index.js:29
+	// Reference: src/blocks/gmap/index.js:71
+	// Reference: src/blocks/unsplash/index.js:31
+	__( '⚠️ You need to provide an API key in ', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/index.js:68
+	__( 'Blocks > Installed Blocks > Card Preview', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/urlfetcher.js:19
+	__( 'Fetching website...', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/urlfetcher.js:28
+	__( '⚠️ Error: ', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/card/urlfetcher.js:57
+	__( 'Paste URL here', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/clicktotweet/index.js:18
+	__( 'Display a nice click to tweet box so your user can easily share your most meaningful sentences', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/clicktotweet/index.js:47
+	__( 'Your tweetable content', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/clicktotweet/index.js:50
+	__( 'Click to tweet', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/clicktotweet/inspect.js:15
+	__( 'Hashtags', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/clicktotweet/inspect.js:18
+	__( 'Add some hashtags (optionnal)', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/clicktotweet/inspect.js:19
+	__( 'Comma separated values, don\'t add #', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/index.js:15
+	__( 'Code', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/index.js:16
+	__( 'Syntax highlighting with custom themes for every languages.', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/index.js:20
+	__( 'highlight', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/index.js:21
+	__( 'syntax', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:17
+	__( 'Settings', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:20
+	__( 'Language', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:33
+	__( 'File name', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:35
+	__( ' /my/optionnal/file/name.ext', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:40
+	__( 'Auto wrap long lines', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:46
+	__( 'Show lines numbers', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:66
+	__( 'Highlight Lines', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:68
+	__( 'Start line', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/code/inspect.js:78
+	__( 'End line', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/index.js:12
+	__( 'Giphy', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/index.js:13
+	__( 'Search and insert a GIF from Giphy.', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/index.js:17
+	__( 'gif', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/index.js:34
+	__( 'Blocks > Manage Blocks > Giphy', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/search.js:104
+	__( 'Trending', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/search.js:135
+	__( 'More GIFs', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/search.js:40
+	// Reference: src/blocks/unsplash/search.js:43
+	__( 'Fetching…', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/search.js:49
+	// Reference: src/blocks/gmap/geocoder.js:28
+	// Reference: src/blocks/unsplash/search.js:52
+	// Reference: src/components/searchplugin/index.js:33
+	// Reference: src/components/searchpost/index.js:27
+	// Reference: src/components/searchproduct/index.js:34
+	__( 'No result', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/search.js:94
+	__( 'Search a GIF', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/giphy/search.js:98
+	// Reference: src/blocks/unsplash/search.js:183
+	__( 'or', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/geocoder.js:20
+	// Reference: src/components/searchplugin/index.js:19
+	// Reference: src/components/searchpost/index.js:20
+	// Reference: src/components/searchproduct/index.js:18
+	__( 'Loading…', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/geocoder.js:34
+	__( 'Geocode was not successful for the following reason:', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/geocoder.js:57
+	__( 'Address', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/geocoder.js:58
+	__( 'Type an address', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/index.js:15
+	__( 'Google Map', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/index.js:16
+	__( 'Display a customizable Google map', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/index.js:20
+	__( 'gmap', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/index.js:76
+	__( 'Blocks > Installed Blocks > Google Map', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/inspect.js:18
+	__( 'Map options', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/inspect.js:23
+	__( 'Marker Popup Title', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/inspect.js:24
+	__( 'My shop Name', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/inspect.js:30
+	__( 'Zoom', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/inspect.js:38
+	__( 'Height', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/gmap/inspect.js:45
+	__( 'Style', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/intro/index.js:11
+	__( 'Intro', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/intro/index.js:12
+	__( 'Display a nice introduction text at the beginning of your post', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/intro/index.js:16
+	__( 'introduction', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/intro/index.js:17
+	__( 'subhead', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/intro/index.js:18
+	__( 'chapo', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/intro/index.js:50
+	__( 'Your introduction here', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/deprecated.js:66
+	// Reference: src/blocks/notice/index.js:20
+	__( 'Advice', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:109
+	__( 'Your tip/warning content', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:25
+	__( 'Information', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:30
+	__( 'Warning', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:35
+	__( 'Avoid', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:43
+	__( 'Notice', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:48
+	__( 'warning', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:49
+	__( 'information', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:50
+	__( 'tips', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:55
+	__( 'Top Line', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/index.js:60
+	__( 'Full', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/inspect.js:15
+	// Reference: src/blocks/post/inspect.js:26
+	__( 'Customize', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/notice/inspect.js:18
+	__( 'Show icon', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/plugin/index.js:14
+	__( 'Plugin', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/plugin/index.js:15
+	__( 'Display informations from a WordPress plugin', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/plugin/index.js:38
+	__( 'Search for a plugin in the inspector', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/plugin/inspect.js:16
+	__( 'Choose a plugin', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/plugin/preview.js:104
+	__( 'Plugin page', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/plugin/preview.js:51
+	__( 'Loading plugin…', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/plugin/preview.js:72
+	// Reference: src/blocks/post/preview.js:98
+	__( 'By', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/plugin/preview.js:96
+	__( 'Active Installations', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/index.js:14
+	__( 'Post', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/index.js:15
+	__( 'Display a pretty post link (from any post type)', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/index.js:57
+	__( 'Search for a post in the inspector', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/inspect.js:17
+	__( 'Choose a post', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/inspect.js:29
+	// Reference: src/blocks/testimonial/inspect.js:30
+	__( 'Show Image?', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/inspect.js:35
+	__( 'Show Author?', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/inspect.js:41
+	__( 'Show Category?', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/preview.js:108
+	__( 'Read more', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/preview.js:114
+	__( 'Loading post…', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/post/preview.js:95
+	__( 'In', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/product/index.js:14
+	__( 'Product', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/product/index.js:15
+	__( 'Display WooCommerce Product in your post', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/product/index.js:19
+	__( 'woocommerce', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/product/index.js:48
+	__( 'Search for a product in the inspector', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/product/inspect.js:22
+	__( 'Choose a product', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/product/inspect.js:36
+	__( 'Price color', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/product/inspect.js:41
+	__( 'Button background color', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/product/preview.js:100
+	__( 'Loading product…', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/summary/index.js:19
+	__( 'Table of contents', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/summary/index.js:20
+	__( 'Display an auto generated, dynamic table of contents', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/summary/index.js:23
+	__( 'summary', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/summary/listitems.js:34
+	__( '(No heading found yet)', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/summary/tools.js:22
+	__( 'Ordered list', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/summary/tools.js:34
+	__( 'Unordered list', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/index.js:115
+	__( 'Write testimonial content here', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/index.js:127
+	__( 'Matt Mullenweg', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/index.js:137
+	__( 'Automattic', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/index.js:14
+	__( 'Testimonial', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/index.js:15
+	__( 'Display a testimonial with a picture, text, name and company', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/index.js:21
+	__( 'review', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/index.js:79
+	__( '× Remove', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/index.js:97
+	__( 'Select Image', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/testimonial/inspect.js:44
+	__( 'Show Company name?', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/index.js:12
+	__( 'Unsplash', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/index.js:13
+	__( 'Find beautiful pictures from the best free photos stock', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/index.js:17
+	__( 'photo', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/index.js:18
+	__( 'picture', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/index.js:19
+	__( 'image', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/index.js:36
+	__( 'Blocks > Manage Blocks > Unsplash', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/search.js:167
+	__( 'Uploading picture in media library, please wait…', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/search.js:179
+	__( 'Search a picture', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/search.js:189
+	__( 'Random', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/search.js:219
+	__( 'More images', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/blocks/unsplash/search.js:87
+	__( 'by', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/components/searchplugin/index.js:41
+	__( '⚠️ Error: Couldn\'t reach wp.org', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/components/searchplugin/index.js:55
+	__( 'Type a plugin name ', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/components/searchpost/index.js:42
+	__( 'Post type', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/components/searchpost/index.js:49
+	__( 'Type a post title', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/components/searchproduct/index.js:25
+	__( 'Sorry, HTTPS is required to search Woo Products', 'advanced-gutenberg-blocks' ),
+
+	// Reference: src/components/searchproduct/index.js:52
+	__( 'Type a product name', 'advanced-gutenberg-blocks' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
