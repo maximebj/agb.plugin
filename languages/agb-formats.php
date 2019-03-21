cfa224e2d7a2cf71a697971c116db5d3612ac93e
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2019-03-21 21:00:17 +0100

    Manually added format string (not auto included in pot for now)

---
+++ languages/agb-formats.php
@@ -0,0 +1,12 @@
+<?php
+
+$generated_i18n_strings = array(
+	
+	__( 'Text color', 'advanced-gutenberg-blocks' ),
+	__( 'Selected Text color', 'advanced-gutenberg-blocks' ),
+	__( 'Remove formatting', 'advanced-gutenberg-blocks' ),
+	__( 'Strike Trough', 'advanced-gutenberg-blocks' ),
+	__( 'Subscript', 'advanced-gutenberg-blocks' ),
+	__( 'Superscript', 'advanced-gutenberg-blocks' )
+
+);
